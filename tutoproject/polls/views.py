9961757fe15bbb3fe1f008522c2a26a592867a69
Rogelio Amaya <Rogelioamayaesp@outlook.com>
2017-02-06 22:51:42 -0700

    Finalizo tutorial 5, introduccion a testing.

--- tutoproject/polls/views.py
+++ tutoproject/polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 #from django.template import loader
 
@@ -11,14 +12,20 @@ from .models import Question, Choice
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
-    context_object_name = 'lasted_question_list'
+    context_object_name = 'latest_question_list'
     
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        #return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
     
 class DetailView(generic.DetailView):
     model = Question
-    template_name = 'polls/detail.html'    
+    template_name = 'polls/detail.html'
+    
+    def get_queryset(self):
+        return Question.objects.filter( pub_date__lte=timezone.now() )
 
 class ResultsView(generic.DetailView):
     model = Question
